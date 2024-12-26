8a5adbd441386ff4afc9f0fb716b07fc4fdf8731
NIXBLACK11 <Siddharthsinghrana11@gmail.com>
2024-12-26 15:12:04 +0530

    setup

---
+++ src/components/CreatePoll.tsx
@@ -0,0 +1,7 @@
+export const CreatePoll = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
---
+++ src/pages/Home.tsx
@@ -0,0 +1,7 @@
+export const Home = () => {
+    return (
+        <div className="w-screen h-screen bg-[#000000] text-[#ffffff]">
+            
+        </div>
+    )
+}
